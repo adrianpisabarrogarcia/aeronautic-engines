bedc706776c8c4077b122c440c4702cafbfd9378
TaNiA <43264054+TaniaGarciaOlarte@users.noreply.github.com>
2020-10-06 12:01:39 +0200

    Delete objetousuario.js

--- objetousuario.js
+++
@@ -1,9 +0,0 @@
-class Usuario{
-    constructor(usu,con,nombre,apellido,dni) {
-        this.usuario = usu;
-        this.contra = con;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.dni = dni;
-    }
-}
